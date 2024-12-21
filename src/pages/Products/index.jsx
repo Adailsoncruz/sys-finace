631d78e745d044e02273f6fce04dfa51eca23d85
unknown <adailsoncruz0213@digitalcollege.internal>
2024-12-21 17:00:55 -0300

    add tabela modal

---
+++ src/pages/Products/index.jsx
@@ -0,0 +1,13 @@
+import Header from "../../components/Header/Header";
+import Modal from "../../components/Modal/modal";
+import Table from "../../components/Table/Table";
+
+export default function Products() {
+    return (
+        <main>
+           <Header/>
+           <Table/>
+           <Modal/>
+        </main>
+    )
+}
